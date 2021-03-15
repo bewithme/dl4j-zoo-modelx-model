b728855bb6f5c15288a9500168a5b21cd45fdf46
bewithmeallmylife@163.com <P@ssw0rd888168>
2021-03-15 13:48:11 +0800

    update something

--- src/main/java/org/freeware/dl4j/modelx/utils/YoloUtils.java
+++ src/main/java/org/freeware/dl4j/modelx/utils/YoloUtils.java
@@ -8,6 +8,14 @@ import org.nd4j.linalg.ops.transforms.Transforms;
 
 public class YoloUtils {
 
+    private static INDArrayIndex[] INDEX_ZERO =new INDArrayIndex[]{NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.point(0)};
+    private static INDArrayIndex[] INDEX_ONE =new INDArrayIndex[]{NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.point(1)};
+    private static INDArrayIndex[] INDEX_TWO =new INDArrayIndex[]{NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.point(2)};
+    private static INDArrayIndex[] INDEX_THREE =new INDArrayIndex[]{NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.point(3)};
+    private static INDArrayIndex[] INDEX_FROM_ZERO_TO_TWO =new INDArrayIndex[]{NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.interval(0,2)};
+    private static INDArrayIndex[] INDEX_FROM_TWO_TO_FOUR =new INDArrayIndex[]{NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.interval(2,4)};
+
+
     /**
      * 获取每个单元格相对于最左上角的坐标
      * 输出形状为[batchSize,gridSize,gridSize,anchorQuantityPerGrid,2]
@@ -51,42 +59,30 @@ public class YoloUtils {
      */
     private  static INDArray getIou(INDArray boundingBoxes1,INDArray boundingBoxes2) {
 
-        INDArrayIndex[] indexZeroToTwo=new INDArrayIndex[]{NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.interval(0,2)};
-
-        INDArrayIndex[] indexTwoToFour=new INDArrayIndex[]{NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.interval(2,4)};
-
-        INDArrayIndex[] indexTwo=new INDArrayIndex[]{NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.point(2)};
-        INDArrayIndex[] indexThree=new INDArrayIndex[]{NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.point(3)};
-
-        INDArrayIndex[] indexZero=new INDArrayIndex[]{NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.point(0)};
-        INDArrayIndex[] indexOne=new INDArrayIndex[]{NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.point(1)};
-
         //计算第一个边界框的面积
-        INDArray boundingBoxes1Area= boundingBoxes1.get(indexTwo).mul(boundingBoxes1.get(indexThree));
+        INDArray boundingBoxes1Area= boundingBoxes1.get(INDEX_TWO).mul(boundingBoxes1.get(INDEX_THREE));
         //计算第二个边界框的面积
-        INDArray boundingBoxes2Area= boundingBoxes2.get(indexTwo).mul(boundingBoxes2.get(indexThree));
+        INDArray boundingBoxes2Area= boundingBoxes2.get(INDEX_TWO).mul(boundingBoxes2.get(INDEX_THREE));
         //左上角坐标
-        INDArray boundingBoxes1LeftTop= boundingBoxes1.get(indexZeroToTwo).sub(boundingBoxes1.get(indexTwoToFour).mul(0.5));
+        INDArray boundingBoxes1LeftTop= boundingBoxes1.get(INDEX_FROM_ZERO_TO_TWO).sub(boundingBoxes1.get(INDEX_FROM_TWO_TO_FOUR).mul(0.5));
         //右下角坐标
-        INDArray boundingBoxes1RightBottom= boundingBoxes1.get(indexZeroToTwo).add(boundingBoxes1.get(indexTwoToFour).mul(0.5));
-
+        INDArray boundingBoxes1RightBottom= boundingBoxes1.get(INDEX_FROM_ZERO_TO_TWO).add(boundingBoxes1.get(INDEX_FROM_TWO_TO_FOUR).mul(0.5));
         //转换为(top x,top y,bottom x, bottom y) 格式的bounding box
         boundingBoxes1=Nd4j.concat(-1,boundingBoxes1LeftTop,boundingBoxes1RightBottom);
 
+        INDArray boundingBoxes2LeftTop= boundingBoxes2.get(INDEX_FROM_ZERO_TO_TWO).sub(boundingBoxes2.get(INDEX_FROM_TWO_TO_FOUR).mul(0.5));
 
-        INDArray boundingBoxes2LeftTop= boundingBoxes2.get(indexZeroToTwo).sub(boundingBoxes2.get(indexTwoToFour).mul(0.5));
-
-        INDArray boundingBoxes2RightBottom= boundingBoxes2.get(indexZeroToTwo).add(boundingBoxes2.get(indexTwoToFour).mul(0.5));
+        INDArray boundingBoxes2RightBottom= boundingBoxes2.get(INDEX_FROM_ZERO_TO_TWO).add(boundingBoxes2.get(INDEX_FROM_TWO_TO_FOUR).mul(0.5));
 
         boundingBoxes2=Nd4j.concat(-1,boundingBoxes2LeftTop,boundingBoxes2RightBottom);
 
-        INDArray boundingBoxesLeftTop= Transforms.max(boundingBoxes1.get(indexZeroToTwo),boundingBoxes2.get(indexZeroToTwo));
+        INDArray boundingBoxesLeftTop= Transforms.max(boundingBoxes1.get(INDEX_FROM_ZERO_TO_TWO),boundingBoxes2.get(INDEX_FROM_ZERO_TO_TWO));
 
-        INDArray boundingBoxesRightBottom=Transforms.min(boundingBoxes1.get(indexTwoToFour),boundingBoxes2.get(indexTwoToFour));
+        INDArray boundingBoxesRightBottom=Transforms.min(boundingBoxes1.get(INDEX_FROM_TWO_TO_FOUR),boundingBoxes2.get(INDEX_FROM_TWO_TO_FOUR));
 
         INDArray interSection=Transforms.max(boundingBoxesRightBottom.sub(boundingBoxesLeftTop),0.0);
 
-        INDArray interArea=interSection.get(indexZero).mul(interSection.get(indexOne));
+        INDArray interArea=interSection.get(INDEX_ZERO).mul(interSection.get(INDEX_ONE));
 
         INDArray unionArea=boundingBoxes1Area.add(boundingBoxes2Area).sub(interArea);
 
@@ -99,59 +95,52 @@ public class YoloUtils {
     /**
      * 批量计算两个边界框的GIOU
      *
-     * @param boundingBoxes1
+     * @param boundingBoxes1 [batchSize,gridSize,gridSize,anchorsSize,(x,y,h,w,confidence,classNum)]
      * @param boundingBoxes2
      * @return
      */
     private  static INDArray getGiou(INDArray boundingBoxes1,INDArray boundingBoxes2) {
 
-        INDArrayIndex[] indexZero=new INDArrayIndex[]{NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.point(0)};
-        INDArrayIndex[] indexOne=new INDArrayIndex[]{NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.point(1)};
-        INDArrayIndex[] indexTwo=new INDArrayIndex[]{NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.point(2)};
-        INDArrayIndex[] indexThree=new INDArrayIndex[]{NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.point(3)};
-        INDArrayIndex[] indexZeroToTwo=new INDArrayIndex[]{NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.interval(0,2)};
-        INDArrayIndex[] indexTwoToFour=new INDArrayIndex[]{NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.all(),NDArrayIndex.interval(2,4)};
         //左上角坐标
-        INDArray boundingBoxes1LeftTop= boundingBoxes1.get(indexZeroToTwo).sub(boundingBoxes1.get(indexTwoToFour).mul(0.5));
+        INDArray boundingBoxes1LeftTop= boundingBoxes1.get(INDEX_FROM_ZERO_TO_TWO).sub(boundingBoxes1.get(INDEX_FROM_TWO_TO_FOUR).mul(0.5));
         //右下角坐标
-        INDArray boundingBoxes1RightBottom= boundingBoxes1.get(indexZeroToTwo).add(boundingBoxes1.get(indexTwoToFour).mul(0.5));
+        INDArray boundingBoxes1RightBottom= boundingBoxes1.get(INDEX_FROM_ZERO_TO_TWO).add(boundingBoxes1.get(INDEX_FROM_TWO_TO_FOUR).mul(0.5));
         //转换为(top x,top y,bottom x, bottom y) 格式的bounding box
         boundingBoxes1=Nd4j.concat(-1,boundingBoxes1LeftTop,boundingBoxes1RightBottom);
 
-        INDArray boundingBoxes2LeftTop= boundingBoxes2.get(indexZeroToTwo).sub(boundingBoxes2.get(indexTwoToFour).mul(0.5));
+        INDArray boundingBoxes2LeftTop= boundingBoxes2.get(INDEX_FROM_ZERO_TO_TWO).sub(boundingBoxes2.get(INDEX_FROM_TWO_TO_FOUR).mul(0.5));
 
-        INDArray boundingBoxes2RightBottom= boundingBoxes2.get(indexZeroToTwo).add(boundingBoxes2.get(indexTwoToFour).mul(0.5));
+        INDArray boundingBoxes2RightBottom= boundingBoxes2.get(INDEX_FROM_ZERO_TO_TWO).add(boundingBoxes2.get(INDEX_FROM_TWO_TO_FOUR).mul(0.5));
         //转换为(top x,top y,bottom x, bottom y) 格式的bounding box
         boundingBoxes2=Nd4j.concat(-1,boundingBoxes2LeftTop,boundingBoxes2RightBottom);
         //确保左上角的坐标小于右下角的坐标
-        boundingBoxes1=Nd4j.concat(-1,Transforms.min(boundingBoxes1.get(indexZeroToTwo),boundingBoxes1.get(indexTwoToFour)),Transforms.max(boundingBoxes1.get(indexZeroToTwo),boundingBoxes1.get(indexTwoToFour)));
+        boundingBoxes1=Nd4j.concat(-1,Transforms.min(boundingBoxes1.get(INDEX_FROM_ZERO_TO_TWO),boundingBoxes1.get(INDEX_FROM_TWO_TO_FOUR)),Transforms.max(boundingBoxes1.get(INDEX_FROM_ZERO_TO_TWO),boundingBoxes1.get(INDEX_FROM_TWO_TO_FOUR)));
         //确保左上角的坐标小于右下角的坐标
-        boundingBoxes2=Nd4j.concat(-1,Transforms.min(boundingBoxes2.get(indexZeroToTwo),boundingBoxes2.get(indexTwoToFour)),Transforms.max(boundingBoxes2.get(indexZeroToTwo),boundingBoxes2.get(indexTwoToFour)));
+        boundingBoxes2=Nd4j.concat(-1,Transforms.min(boundingBoxes2.get(INDEX_FROM_ZERO_TO_TWO),boundingBoxes2.get(INDEX_FROM_TWO_TO_FOUR)),Transforms.max(boundingBoxes2.get(INDEX_FROM_ZERO_TO_TWO),boundingBoxes2.get(INDEX_FROM_TWO_TO_FOUR)));
         //计算第一个边界框的面积
-        INDArray boundingBoxes1Area= boundingBoxes1.get(indexTwo).sub(boundingBoxes1.get(indexZero)).mul(boundingBoxes1.get(indexThree).add(boundingBoxes1.get(indexOne)));
+        INDArray boundingBoxes1Area= boundingBoxes1.get(INDEX_TWO).sub(boundingBoxes1.get(INDEX_ZERO)).mul(boundingBoxes1.get(INDEX_THREE).add(boundingBoxes1.get(INDEX_ONE)));
         //计算第二个边界框的面积
-        INDArray boundingBoxes2Area=boundingBoxes2.get(indexTwo).sub(boundingBoxes2.get(indexZero)).mul(boundingBoxes2.get(indexThree).add(boundingBoxes2.get(indexOne)));
+        INDArray boundingBoxes2Area=boundingBoxes2.get(INDEX_TWO).sub(boundingBoxes2.get(INDEX_ZERO)).mul(boundingBoxes2.get(INDEX_THREE).add(boundingBoxes2.get(INDEX_ONE)));
 
-        INDArray boundingBoxesLeftTop= Transforms.max(boundingBoxes1.get(indexZeroToTwo),boundingBoxes2.get(indexZeroToTwo));
+        INDArray boundingBoxesLeftTop= Transforms.max(boundingBoxes1.get(INDEX_FROM_ZERO_TO_TWO),boundingBoxes2.get(INDEX_FROM_ZERO_TO_TWO));
 
-        INDArray boundingBoxesRightBottom=Transforms.min(boundingBoxes1.get(indexTwoToFour),boundingBoxes2.get(indexTwoToFour));
+        INDArray boundingBoxesRightBottom=Transforms.min(boundingBoxes1.get(INDEX_FROM_TWO_TO_FOUR),boundingBoxes2.get(INDEX_FROM_TWO_TO_FOUR));
 
         INDArray interSection=Transforms.max(boundingBoxesRightBottom.sub(boundingBoxesLeftTop),0.0);
 
-        INDArray interArea=interSection.get(indexZero).mul(interSection.get(indexOne));
+        INDArray interArea=interSection.get(INDEX_ZERO).mul(interSection.get(INDEX_ONE));
 
         INDArray unionArea=boundingBoxes1Area.add(boundingBoxes2Area).sub(interArea);
 
         INDArray iou=interArea.mul(1.0).mul(unionArea);
         //计算boxes1和boxes2的最小凸集框的左上角和右下角坐标
-        INDArray encloseBoundingBoxesLeftTop= Transforms.min(boundingBoxes1.get(indexZeroToTwo),boundingBoxes2.get(indexZeroToTwo));
+        INDArray encloseBoundingBoxesLeftTop= Transforms.min(boundingBoxes1.get(INDEX_FROM_ZERO_TO_TWO),boundingBoxes2.get(INDEX_FROM_ZERO_TO_TWO));
 
-        INDArray encloseBoundingBoxesRightBottom=Transforms.max(boundingBoxes1.get(indexTwoToFour),boundingBoxes2.get(indexTwoToFour));
-
+        INDArray encloseBoundingBoxesRightBottom=Transforms.max(boundingBoxes1.get(INDEX_FROM_TWO_TO_FOUR),boundingBoxes2.get(INDEX_FROM_TWO_TO_FOUR));
         //计算最小凸集的边长
         INDArray enclose=Transforms.max(encloseBoundingBoxesRightBottom.sub(encloseBoundingBoxesLeftTop),0);
         //计算最小凸集的面积
-        INDArray encloseArea=enclose.get(indexZero).mul(enclose.get(indexOne));
+        INDArray encloseArea=enclose.get(INDEX_ZERO).mul(enclose.get(INDEX_ONE));
         //【最小凸集内不属于两个框的区域】与【最小凸集】的比值
         INDArray rate=encloseArea.sub(unionArea).mul(1.0).div(encloseArea);
 
