14295cb06da7a82c5b2488d2dd64af70fe13351c
bewithmeallmylife@163.com <P@ssw0rd888168>
2021-03-23 17:57:06 +0800

    update something

--- src/main/java/org/freeware/dl4j/modelx/dataset/Yolo3DataSetIterator.java
+++ src/main/java/org/freeware/dl4j/modelx/dataset/Yolo3DataSetIterator.java
@@ -324,18 +324,9 @@ public class Yolo3DataSetIterator implements MultiDataSetIterator {
 	}
 
 	private void setExtraValues(int exampleCount, INDArray boxesCount, int labelIndex, INDArray scaledBoundingBox, INDArray label) {
-
 		int bigMediumSmallScaledBoundingBoxIndex=boxesCount.get(new INDArrayIndex[]{NDArrayIndex.point(labelIndex)}).toIntVector()[0];
 		int priorBoundingBoxIndex=bigMediumSmallScaledBoundingBoxIndex%maxBoxPerImage;
-		long gridWidth=label.shape()[1];
-		long gridHeight=label.shape()[2];
-		/*for (int gridWidthIndex=0;gridWidthIndex<gridWidth;gridWidthIndex++){
-			for (int gridHeightIndex=0;gridHeightIndex<gridHeight;gridHeightIndex++){
-				label.put(new INDArrayIndex[]{NDArrayIndex.point(exampleCount),NDArrayIndex.point(gridWidthIndex),NDArrayIndex.point(gridHeightIndex),NDArrayIndex.point(numberOfPriorBoundingBoxPerGridCell+priorBoundingBoxIndex),NDArrayIndex.interval(0,4)},scaledBoundingBox);
-			}
-		}*/
 		label.put(new INDArrayIndex[]{NDArrayIndex.point(exampleCount),NDArrayIndex.point(0),NDArrayIndex.point(0),NDArrayIndex.point(numberOfPriorBoundingBoxPerGridCell+priorBoundingBoxIndex),NDArrayIndex.interval(0,4)},scaledBoundingBox);
-
 		bigMediumSmallScaledBoundingBoxIndex++;
 		boxesCount.put(new INDArrayIndex[]{NDArrayIndex.point(labelIndex)},bigMediumSmallScaledBoundingBoxIndex);
 	}
