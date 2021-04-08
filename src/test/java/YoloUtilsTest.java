126828249bd99187be0f6614ffbb7160e9ab5250
bewithmeallmylife@163.com <P@ssw0rd888168>
2021-04-08 17:32:25 +0800

    update something

---
+++ src/test/java/YoloUtilsTest.java
@@ -0,0 +1,67 @@
+import lombok.extern.slf4j.Slf4j;
+import org.freeware.dl4j.modelx.utils.INDArrayUtils;
+import org.freeware.dl4j.modelx.utils.YoloUtils;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+
+
+@Slf4j
+public class YoloUtilsTest {
+
+
+
+
+
+
+    public  static  void main(String[] args){
+
+
+        testGradientOfOfGIou();
+    }
+
+
+    private static void testGradientOfOfGIou() {
+
+        INDArray labels= Nd4j.create(1,1,1,1,4);
+
+
+        labels.put(INDArrayUtils.getLastDimensionPointZero(labels),21);
+
+        labels.put(INDArrayUtils.getLastDimensionPointOne(labels),45);
+
+        labels.put(INDArrayUtils.getLastDimensionPointTwo(labels),103);
+
+        labels.put(INDArrayUtils.getLastDimensionPointThree(labels),172);
+
+        INDArray predict=  Nd4j.create(1,1,1,1,4);
+
+        predict.put(INDArrayUtils.getLastDimensionPointZero(labels),59);
+
+        predict.put(INDArrayUtils.getLastDimensionPointOne(labels),106);
+
+        predict.put(INDArrayUtils.getLastDimensionPointTwo(labels),154);
+
+        predict.put(INDArrayUtils.getLastDimensionPointThree(labels),230);
+
+        INDArray gIou= YoloUtils.getGradientOfIou(predict,labels);
+
+
+        log.info(gIou.toString());
+
+    }
+
+    private static void testGradientOfOfFocal() {
+
+        INDArray labels= Nd4j.linspace(0,11,12);
+
+        INDArray predict= Nd4j.linspace(3,14,12);
+
+        INDArray loss= YoloUtils.focal(labels,predict);
+
+        INDArray gradientOfLoss=YoloUtils.gradientOfOfFocal(labels,predict);
+
+        log.info(loss.toString());
+
+        log.info(gradientOfLoss.toString());
+    }
+}
