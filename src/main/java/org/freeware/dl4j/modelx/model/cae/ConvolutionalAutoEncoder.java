7119842f4c47b4f1d5609c8d480cf46db0129401
bewithmeallmylife@163.com <P@ssw0rd888168>
2021-05-14 11:20:50 +0800

    update something

---
+++ src/main/java/org/freeware/dl4j/modelx/model/cae/ConvolutionalAutoEncoder.java
@@ -0,0 +1,334 @@
+package org.freeware.dl4j.modelx.model.cae;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.extern.slf4j.Slf4j;
+import org.deeplearning4j.nn.api.Model;
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.*;
+import org.deeplearning4j.nn.conf.inputs.InputType;
+import org.deeplearning4j.nn.conf.layers.*;
+import org.deeplearning4j.nn.conf.preprocessor.CnnToFeedForwardPreProcessor;
+import org.deeplearning4j.nn.conf.preprocessor.FeedForwardToCnnPreProcessor;
+import org.deeplearning4j.nn.graph.ComputationGraph;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.zoo.ModelMetaData;
+import org.deeplearning4j.zoo.PretrainedType;
+import org.freeware.dl4j.modelx.model.ZooModelX;
+import org.freeware.dl4j.nn.GraphLayerItem;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.learning.config.Adam;
+import org.nd4j.linalg.learning.config.IUpdater;
+import org.nd4j.linalg.learning.config.Sgd;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
+@AllArgsConstructor
+@Builder
+@Slf4j
+public class ConvolutionalAutoEncoder extends ZooModelX {
+
+    @Builder.Default
+    private long seed = 12345;
+
+    @Builder.Default
+    private WorkspaceMode workspaceMode = WorkspaceMode.ENABLED;
+
+    private static final double LEARNING_RATE = 0.0003;
+
+    private static final double GRADIENT_THRESHOLD = 100.0;
+
+    private static final IUpdater UPDATER_ZERO = Sgd.builder().learningRate(0.0).build();
+
+    private static int DISCRIMINATOR_INPUT_SIZE = 784;
+
+    private static int GENERATOR_INPUT_SIZE = 100;
+
+    private static int LATENT_DIM_LEN = 100;
+
+
+
+    @Builder.Default
+    private int imageHeight = 28;
+
+    @Builder.Default
+    private int imageWidth = 28;
+
+    @Builder.Default
+    private int imageChannel = 3;
+
+    @Builder.Default
+    private IUpdater updater = Adam.builder().learningRate(LEARNING_RATE).beta1(0.5).build();
+
+
+    private static Random random = new Random(123456);
+
+
+    /**
+     * 生成器网络配置
+     *
+     * @return
+     */
+    public ComputationGraphConfiguration buildEncoderConfiguration() {
+
+        ComputationGraphConfiguration.GraphBuilder graph = new NeuralNetConfiguration.Builder().seed(seed)
+                .gradientNormalization(GradientNormalization.RenormalizeL2PerLayer)
+                .gradientNormalizationThreshold(GRADIENT_THRESHOLD)
+                .weightInit(WeightInit.XAVIER)
+                .activation(Activation.IDENTITY)
+                .trainingWorkspaceMode(workspaceMode)
+                .inferenceWorkspaceMode(workspaceMode)
+                .convolutionMode(ConvolutionMode.Same)
+                .graphBuilder();
+
+        String[] inputs = {"input"};
+
+        List<GraphLayerItem> encoderLayerItems= buildEncoderGraphLayerItems(inputs,updater);
+
+        addGraphItems(graph,encoderLayerItems,false);
+
+        String outputLayerName=getLastLayerName(encoderLayerItems);
+
+        graph.addInputs(inputs);
+
+        graph.setOutputs(outputLayerName);
+
+        return graph.build();
+    }
+
+
+
+
+
+
+    public ComputationGraphConfiguration buildAutoEncoder() {
+
+        ComputationGraphConfiguration.GraphBuilder graph = new NeuralNetConfiguration.Builder().seed(seed)
+
+                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+                .updater(updater)
+                .gradientNormalization(GradientNormalization.RenormalizeL2PerLayer)
+                .gradientNormalizationThreshold(GRADIENT_THRESHOLD)
+                .weightInit(WeightInit.XAVIER)
+                .activation(Activation.TANH)
+                .updater(updater)
+                .trainingWorkspaceMode(workspaceMode)
+                .inferenceWorkspaceMode(workspaceMode)
+                .convolutionMode(ConvolutionMode.Same)
+                .graphBuilder();
+
+        String[] inputs = {"input"};
+
+        List<GraphLayerItem> encoderLayerItems= buildEncoderGraphLayerItems(inputs,updater);
+
+        addGraphItems(graph,encoderLayerItems,false);
+
+        String decoderInputName=getLastLayerName(encoderLayerItems);
+
+        List<GraphLayerItem> decoderLayerItems= buildDecoderGraphLayerItems(new String[]{decoderInputName});
+
+        addGraphItems(graph,decoderLayerItems,false);
+
+        String output=getLastLayerName(decoderLayerItems);
+
+        graph.addInputs(inputs);
+
+        graph.setOutputs(output);
+
+        graph.setInputTypes(InputType.convolutional(imageHeight, imageWidth, imageChannel));
+
+        return graph.build();
+    }
+
+
+    /**
+     * 解码器层列表
+     *
+     * @param inputs
+     * @return
+     */
+    private List<GraphLayerItem> buildDecoderGraphLayerItems(String[] inputs) {
+
+        List<GraphLayerItem> graphItemList = new ArrayList<GraphLayerItem>(10);
+
+        graphItemList.add(new GraphLayerItem("de-01",
+                new Deconvolution2D.Builder()
+                        .nOut(32)
+                        .kernelSize(3, 3)
+                        .stride(2, 2)
+                        .build(),
+                inputs));
+
+        graphItemList.add(new GraphLayerItem("de-02",
+                new Deconvolution2D.Builder()
+                        .nOut(32)
+                        .kernelSize(3, 3)
+                        .stride(2, 2)
+                        .build(),
+                new String[]{"de-01"}));
+
+        graphItemList.add(new GraphLayerItem("de-03",
+                new Deconvolution2D.Builder()
+                        .nOut(32)
+                        .kernelSize(3, 3)
+                        .stride(2, 2)
+                        .build(),
+                new String[]{"de-02"}));
+
+        graphItemList.add(new GraphLayerItem("de-04",
+                new Deconvolution2D.Builder()
+                        .nOut(3)
+                        .kernelSize(3, 3)
+                        .stride(2, 2)
+                        .build(),
+                new String[]{"de-03"}));
+
+
+        graphItemList.add(new GraphLayerItem("output",
+                new CnnLossLayer.Builder(LossFunctions.LossFunction.MSE)
+                        .activation(Activation.TANH).build(),
+                new String[]{"de-04"}));
+
+
+
+
+        return graphItemList;
+
+    }
+
+
+
+
+
+    /**
+     * 编码器
+     * @param inputs
+     *
+     * @return
+     */
+    private  List<GraphLayerItem> buildEncoderGraphLayerItems(String[] inputs,IUpdater updater){
+
+        List<GraphLayerItem>  graphItemList=new ArrayList<GraphLayerItem>(10);
+
+        graphItemList.add(new GraphLayerItem("en-01",
+                new Convolution2D.Builder()
+                        .nIn(imageChannel)
+                        .kernelSize(3,3)
+                        .stride(2,2)
+                        .updater(updater)
+                        .nOut(32)
+                        .build(),
+                 inputs));
+
+        graphItemList.add(new GraphLayerItem("en-02",
+                new SubsamplingLayer.Builder(
+                        SubsamplingLayer.PoolingType.MAX,
+                        new int[]{2,2},
+                        new int[]{ 2,2})
+
+                        .build(),
+                new String[]{"en-01"}));
+
+
+        graphItemList.add(new GraphLayerItem("en-03",
+                new Convolution2D.Builder()
+                        .nIn(32)
+                        .kernelSize(3,3)
+                        .stride(2,2)
+                        .nOut(32)
+                        .updater(updater)
+                       .build(),
+                new String[]{"en-02"}));
+
+        graphItemList.add(new GraphLayerItem("en-04",
+                new SubsamplingLayer.Builder(
+                        SubsamplingLayer.PoolingType.MAX,
+                        new int[]{2,2},
+                        new int[]{ 2,2})
+                        .build(),
+                new String[]{"en-03"}));
+
+      //shape= [N,32,16,16] if conMode=same outW=floor(inW/stride)
+
+        return graphItemList;
+
+    }
+
+
+    @Override
+    public void setInputShape(int[][] inputShape) {
+
+    }
+
+
+    /**
+     * 初始化对抗网络
+     * @return
+     */
+    public ComputationGraph init() {
+
+        ComputationGraphConfiguration configuration= buildAutoEncoder();
+
+        ComputationGraph gan = new ComputationGraph(configuration);
+
+        gan.init();
+
+        return gan;
+
+    }
+
+    /**
+     * 初始化生成器
+     * @return
+     */
+    public ComputationGraph initEncoder() {
+
+        ComputationGraphConfiguration genConf= buildEncoderConfiguration();
+
+        ComputationGraph model = new ComputationGraph(genConf);
+
+        model.init();
+
+        return model;
+    }
+
+
+
+
+    /**
+     * 复制两个除输出层之外的两个网络的参数
+     * @param from
+     * @param to
+     */
+    public void copyParamsWithoutOutputLayer(ComputationGraph from, ComputationGraph to) {
+        int genLayerLen = from.getLayers().length-1;
+        for (int i = 0; i < genLayerLen; i++) {
+            to.getLayer(i).setParams(from.getLayer(i).params());
+        }
+    }
+
+
+    @Override
+    public ModelMetaData metaData() {
+        return null;
+    }
+
+    @Override
+    public Class<? extends Model> modelType() {
+        return null;
+    }
+
+    @Override
+    public String pretrainedUrl(PretrainedType pretrainedType) {
+        return null;
+    }
+
+    @Override
+    public long pretrainedChecksum(PretrainedType pretrainedType) {
+        return 0;
+    }
+}
---
+++ src/main/java/org/freeware/dl4j/modelx/train/cae/ConvolutionalAutoEncoderTrainer.java
@@ -0,0 +1,174 @@
+package org.freeware.dl4j.modelx.train.cae;
+
+import lombok.extern.slf4j.Slf4j;
+import org.deeplearning4j.nn.graph.ComputationGraph;
+import org.freeware.dl4j.modelx.model.cae.ConvolutionalAutoEncoder;
+import org.freeware.dl4j.modelx.model.gan.SGan;
+import org.freeware.dl4j.modelx.train.gan.AbsGanTrainer;
+import org.freeware.dl4j.modelx.utils.DataSetUtils;
+import org.freeware.dl4j.modelx.utils.Sample;
+import org.freeware.dl4j.modelx.utils.SplitDataSet;
+import org.freeware.dl4j.modelx.utils.VisualisationUtils;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
+import org.nd4j.linalg.dataset.api.preprocessor.ImagePreProcessingScaler;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.learning.config.Adam;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+
+
+/**
+ * @author wenfengxu
+ *
+ */
+@Slf4j
+public class ConvolutionalAutoEncoderTrainer extends AbsGanTrainer {
+
+
+    private  static  DataNormalization dataNormalization = new ImagePreProcessingScaler(-1,1);
+
+    private static String outputDir="output_CAE";
+
+    public static void main(String[] args) {
+
+        int batchSize=2;
+
+        int imageHeight =256;
+
+        int imageWidth =256;
+
+        int imageChannel =3;
+
+
+        String dataPath="/Users/wenfengxu/Downloads/data/mnist_png/training";
+
+        int numClasses=10;
+
+        ConvolutionalAutoEncoder cae= ConvolutionalAutoEncoder.builder()
+                .imageChannel(imageChannel)
+                .imageHeight(imageHeight)
+                .imageWidth(imageWidth)
+                .updater(Adam.builder()
+                        .learningRate(0.0003)
+                        .beta1(0.5).build()
+                 )
+                .build();
+
+        //初始化无监叔判别器
+        ComputationGraph caeGraph=cae.init();
+
+        ComputationGraph encoderGraph=cae.initEncoder();
+
+        //设置训练监听器
+        setListeners(caeGraph,encoderGraph);
+
+        log.info(caeGraph.summary());
+
+        Nd4j.getMemoryManager().setAutoGcWindow(15 * 1000);
+
+        DataSetIterator trainData = null;
+
+        try {
+
+            trainData= DataSetUtils.getDataSetIterator(dataPath,batchSize,numClasses,imageHeight,imageWidth,imageChannel);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        int iterationCounter = 0;
+
+        while (true) {
+
+            trainData.reset();
+
+            while (trainData.hasNext()) {
+
+                iterationCounter++;
+
+                DataSet dataSet=trainData.next();
+
+                INDArray realFeature = dataSet.getFeatures();
+
+                DataSet caeDataSet=new DataSet(realFeature,realFeature);
+
+                caeGraph.fit(caeDataSet);
+
+            }
+
+        }
+
+    }
+
+    private static void evaluate(ComputationGraph superviseDiscriminator, DataSetIterator testDataSetIterator, int iterationCounter) {
+
+        if (iterationCounter % 100== 0) {
+
+           log.info(superviseDiscriminator.evaluate(testDataSetIterator).stats());
+
+        }
+    }
+
+    /**
+     * 可视化
+     * @param generator
+     * @param iterationCounter
+     */
+    private static void visualize(ComputationGraph generator, int iterationCounter) {
+
+        Sample[] samples=null;
+
+        if (iterationCounter % 10== 0) {
+
+            samples=getSamples(generator);
+
+            VisualisationUtils.mnistVisualizeForConvolution2D(samples,"SGan");
+        }
+        if (iterationCounter % 1000== 0) {
+
+            String savePath=outputDir.concat(File.separator).concat(String.valueOf(iterationCounter));
+
+            VisualisationUtils.saveAsImageForConvolution2D(samples,savePath);
+        }
+    }
+
+
+    /**
+     * 采样生成器
+     * 9个输出
+     * @param generator
+     * @return
+     */
+    private static Sample[] getSamples(ComputationGraph generator) {
+
+        int batchSize=1;
+
+        Sample[] samples = new Sample[9];
+
+        for(int k=0;k<9;k++){
+            //创建batchSize行，100列的随机数浅层空间
+            INDArray latentDim = Nd4j.rand(new int[]{batchSize,  100});
+            //输出图片
+            INDArray fakeImage=generator.output(latentDim)[0];
+            //把图片数据恢复到0-255
+            dataNormalization.revertFeatures(fakeImage);
+
+            Sample sample=new Sample(fakeImage,"");
+
+            samples[k]=sample;
+        }
+        return samples;
+    }
+
+
+
+
+
+
+
+}
